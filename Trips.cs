c2360a72cb6a51d3de886b16690b9f9d3d28ba7b
Azat <46015824+serikovskii@users.noreply.github.com>
2019-01-27 19:51:22 +0600

    Create Trips.cs

---
+++ Trips.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusValidator
+{
+    public class Trips
+    {
+        public string TypeBus { get; set; }
+        public int PriceTripBus { get; set; }
+        public int SaleForTripBus { get; set; }
+
+        public Trips()
+        {
+            PriceTripBus = 0;
+            SaleForTripBus = 0;
+        }
+    }
+}
