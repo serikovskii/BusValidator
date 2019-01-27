a44400a114abdbbfa342500344220d2465fa1850
Azat <46015824+serikovskii@users.noreply.github.com>
2019-01-27 19:51:26 +0600

    Create Cards.cs

---
+++ Cards.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusValidator
+{
+    public class Cards
+    {
+        public string TypeCard { get; set; }
+        public int BalansCard { get; set; }
+        public int IdCard { get; set; }
+        public Cards()
+        {
+            IdCard = 0;
+            BalansCard = 0;
+        }
+    }
+}
